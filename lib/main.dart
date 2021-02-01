5e934d07e72b3dfc17812fa46a4a7e46518fbef0
JohnR46 <Johnnote7@gmail.com>
2021-02-01 21:55:47 +0700

    add picture

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "MY app",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
-        ),
-        body: Text('สวัสดีครับ flutter '),
-      ),
+          appBar: AppBar(
+            title: Text(
+              'Hello Flutter',
+            ),
+          ),
+          body: Center(
+              child: Image(
+            image: NetworkImage(
+                "https://blog.photofeeler.com/wp-content/uploads/2017/09/tinder-photo-size-tinder-picture-size-tinder-aspect-ratio-image-dimensions-crop.jpg"),
+          ))),
       theme: ThemeData(primaryColor: Colors.redAccent),
     );
   }
